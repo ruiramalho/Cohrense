494e11cdc3354b3693ba7d97aaef490ebac66c09
Rui Ramalho <ruimcramalho@gmail.com>
2019-03-26 15:32:14 +0000

    teste

--- Test Suites/Coherense Project/Coherense Test Suite.ts
+++ Test Suites/Coherense Project/Coherense Test Suite.ts
@@ -4,10 +4,16 @@
    <name>Coherense Test Suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <mailRecipient></mailRecipient>
+   <mailRecipient>ruimcramalho@gmail.com;</mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>2e08a3cf-ae2e-440f-ae7e-c0521fe99d42</testSuiteGuid>
+   <testCaseLink>
+      <guid>83a7c82e-5537-4f4a-8f04-2feb68747b95</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Coherense</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
